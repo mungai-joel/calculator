29b5e0c3320e58b02331c4f330b44177084292b6
joel <mungaijoel123@gmail.com>
2020-02-13 12:39:38 +0300

    css and js files

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var add = function(numb1,numb2){
+    return(numb1 + numb2);
+    }
+    numb1=parseInt(prompt("enter a number ..."));
+    numb2=parseInt(prompt("enter another number..."));
+    alert(add(numb1,numb2));
+    
